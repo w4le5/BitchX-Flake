177a32d2aedea094f44e5b9c63d5230c9117a3f1
wa'leS <112171624+w4le5@users.noreply.github.com>
2026-06-15 16:11:41 +0000

    Updated for 26.05

--- flake.nix
+++ flake.nix
@@ -1,69 +1,52 @@
-{
-  description = "BitchX IRC client";
+{ stablePkgs }:
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
+stablePkgs.stdenv.mkDerivation {
+	pname = "bitchx";
+	version = "1.3";
 
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
-      ];
-    in {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "bitchx";
-            version = "1.3";
+	src = stablePkgs.fetchFromGitHub {
+		owner = "BitchX";
+		repo = "BitchX1.3";
+		rev = "master";
+		sha256 = "0i5wk09h6p9aip9abw147y9xsgsi718vbw2ydmvm94mcfbs7q8gd";
+	};
 
-            src = pkgs.fetchFromGitHub {
-              owner = "BitchX";
-              repo = "BitchX1.3";
-              rev = "master";
-              sha256 = "sha256-oqPWQ6YH6tz6RQwl2rjRUZK6IlpgxPjIJS1YBl1myVE=";
-            };
+	dontSetConfigureFlags = true;
 
-            dontSetConfigureFlags = true;
-            nativeBuildInputs = with pkgs; [ autoconf automake libtool pkg-config gcc gnumake gettext cpio ];
-            buildInputs = with pkgs; [ ncurses openssl zlib libxcrypt ];
-            hardeningDisable = [ "fortify" ];
+	nativeBuildInputs = with stablePkgs; [
+		autoconf automake libtool pkg-config gnumake gettext cpio
+	];
 
-            NIX_CFLAGS_COMPILE = "-Wno-error=format-security -Wno-error=stringop-overflow";
+	buildInputs = with stablePkgs; [
+		ncurses openssl zlib libxcrypt
+	];
 
-            configurePhase = ''
-              if [ -x ./autogen.sh ]; then
-                ./autogen.sh
-              else
-                autoreconf -fi
-              fi
+	hardeningDisable = [ "fortify" ];
 
-              find . -type f -name "Makefile*" -exec sed -i 's/-Werror//g' {} +
+	NIX_CFLAGS_COMPILE = "-Wno-error=format-security -Wno-error=stringop-overflow";
 
-              ./configure --prefix=$out --with-ssl --enable-ipv6 LDFLAGS="-lcrypt"
-            '';
+	configurePhase = ''
+		if [ -x ./autogen.sh ]; then
+			./autogen.sh
+		else
+			autoreconf -fi
+		fi
 
-            buildPhase = ''
-              make LDFLAGS="-lcrypt"
-            '';
+		find . -type f -name "Makefile*" -exec sed -i 's/-Werror//g' {} +
 
-            installPhase = "make install";
+		./configure --prefix=$out --with-ssl --enable-ipv6 LDFLAGS="-lcrypt"
+	'';
 
-            meta = with pkgs.lib; {
-              description = "Classic terminal-based IRC client (ircII fork)";
-              homepage = "https://github.com/BitchX/BitchX1.3";
-              license = licenses.gpl2Plus;
-              maintainers = [];
-              platforms = platforms.unix;
-            };
-          };
-        });
+	buildPhase = ''
+		make LDFLAGS="-lcrypt"
+	'';
 
-      # overlay
-      overlay = final: prev: {
-        bitchx = self.packages.${final.system}.default;
-      };
-    };
+	installPhase = "make install";
+
+	meta = with stablePkgs.lib; {
+		description = "Classic terminal-based IRC client (ircII fork)";
+		homepage = "https://github.com/BitchX/BitchX1.3";
+		license = licenses.gpl2Plus;
+		platforms = platforms.unix;
+	};
 }
